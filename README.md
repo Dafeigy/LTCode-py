f6713d24fc0a35198f9c3b0ef0099725c32be224
Dafeigy <1070642765@qq.com>
2024-10-25 17:05:03 +0800

    <Update>: Finish LT-Encoder and sampler. Utils will be removed latter since it's only functioning in logging info.

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 ## What is this
 
-This is a python implementation of LT-code, which contains encode & decode procesdures.
+This is a python implementation of **Luby Transform code**, which contains encode & decode procesdures. 
+Encoder and Decoder are reconstructed based on [@Anrosent](https://github.com/anrosent/LT-code)'s implementation, many thanks!
 
 ~~It will be used in a future project(Haven't decided name yet), which relies on no internet but only camera device to receive data from one device to another.~~
+
+## Progress
+
+- [ ] LT Encoder: Key function and logics are done and tested. 
+
+- [ ] LT Decoder: Still studying the decode process...
+
+- [ ] Protobuf supported. This can be done in a very short time.
+
 
 To be continued ...
---
+++ encoder.py
@@ -0,0 +1,71 @@
+import sys
+from random import randint
+from struct import pack
+import time
+from utils import prCyan
+
+from io import BufferedReader
+
+import sampler
+
+
+class LTEncoder():
+    def __init__(self, blocksize) -> None:
+        self.blocksize = blocksize
+        pass
+
+    def _split_file(self, source:BufferedReader | str):
+        """
+        Block file byte contents into blocksize chunks, padding last one if necessary
+        """
+        # TODO: Maybe f can be str or File Instance?
+        if isinstance(source, BufferedReader):
+            f_bytes = f.read()
+        elif isinstance(source, str):
+            f_bytes = str.encode(source)
+        else:
+            raise TypeError("Only BufferedReader or str supported.")
+    
+        # print(type(f_bytes),type(f))
+        blocks = [int.from_bytes(f_bytes[i:i+self.blocksize].ljust(self.blocksize, b'0'), sys.byteorder) 
+                for i in range(0, len(f_bytes), self.blocksize)]
+        return len(f_bytes), blocks
+
+    def _gen_blocks(self, source, seed=None, c=sampler.DEFAULT_C, delta=sampler.DEFAULT_DELTA):
+        """Generates an infinite sequence of blocks to transmit
+        to the receiver
+        """
+
+        # Generate seed if not provided
+        if seed is None:
+            seed = randint(0, 1 << 31 - 1)
+        # get file blocks
+        filesize, blocks = self._split_file(source)
+
+        # init stream vars
+        K = len(blocks)
+        prng = sampler.PRNG(params=(K, delta, c))
+        prng.set_seed(32)
+
+        # block generation loop
+        while True:
+            blockseed, d, ix_samples = prng.get_src_blocks()
+            block_data = 0
+            for ix in ix_samples:
+                block_data ^= blocks[ix]
+            print("======================================================")
+            print(f"File size: {filesize}, Block Size: {self.blocksize}, Blockseed: {blockseed}")
+            
+            # Generate blocks of XORed data in network byte order
+            block = (filesize, self.blocksize, blockseed, int.to_bytes(block_data, self.blocksize, sys.byteorder))
+            prCyan(block)
+            yield pack('!III%ss'%self.blocksize, *block)
+
+if __name__ == "__main__":
+    lteconder = LTEncoder(32)
+    test_str = "This is an implementation of a Luby Transform code in Python, consisting of two executables, one for each encoding and decoding files. These are thin wrappers around a core stream/file API."
+    with open ("test.txt",'rb') as f:
+        for block in lteconder._gen_blocks(f):
+            # pass
+            time.sleep(1)
+
